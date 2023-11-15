9199b53385a2faaacfd311d9b66031e76cf9fec2
agusgarcia3007 <agusgarcia3007@gmail.com>
2023-11-15 00:58:04 -0300

    :wrench: Fix next.config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["apod.nasa.gov", "mars.jpl.nasa.gov"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "mars.nasa.gov",
+      },
+      {
+        protocol: "https",
+        hostname: "www.jpl.nasa.gov",
+      },
+    ],
   },
 };
 
